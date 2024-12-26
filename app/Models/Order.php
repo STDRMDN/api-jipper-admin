0e0140471f2cffdd50dc5193f9a8499307229f23
Setiadi Ramdani <140574639+STDRMDN@users.noreply.github.com>
2024-12-27 01:06:26 +0700

    update models\order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,27 @@ class Order extends Model
 {
     use HasFactory;
 
+
+    protected $fillable = [
+        'status',
+        'email',
+        'country',
+        'name',
+        'address',
+        'address_detail',
+        'city',
+        'province',
+        'postal_code',
+        'phone',
+        'shipping_method',
+        'subtotal',
+        'shipping_fee',
+        'tax',
+        'total',
+        'is_product',
+        'id_dyo'
+    ];
+
     protected $guarded = [];
 
     public function productOrders()
