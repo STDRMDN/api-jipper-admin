7dfb07b5c6fb83cdcf4d12a4a40a1fcb4f99ecbd
Setiadi Ramdani <140574639+STDRMDN@users.noreply.github.com>
2025-01-25 00:36:01 +0700

    updater

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\DyoResource;
 
 class ProductController extends Controller
 {
